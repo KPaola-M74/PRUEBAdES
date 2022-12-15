a2afc6020f5d62991241921b7d03ec14d3e63ce2
DESKTOP-OLNM4V6\Juan Diego <juandiegotres@hotmail.com>
2022-12-15 15:34:42 -0500

    Se conectó django con react

--- Backend/Wattpay/urls.py
+++ Backend/Wattpay/urls.py
@@ -1,4 +1,4 @@
-"""Backend URL Configuration
+"""Wattpay URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from loginPage.views import front
 from django.urls import path, include
-from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-
+    path('admin/', admin.site.urls),
+    path("", front, name ="front")
+    #path('api-auth/', include('rest_framework.urls')),
 ]
